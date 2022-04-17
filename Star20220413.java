2b3b866e2d9e52b33226da3c0d7dac7086674878
ESSAANNGG <tkdwnsdk11@gmail.com>
2022-04-17 23:04:46 +0900

    ,,,

--- Star20220413.java
+++ Star20220413.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Star20220413 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("숫자를 입력하시오.");
         int a = sc.nextInt();
         int b = sc.nextInt();
         for(int i = 0; i < b; i++){
