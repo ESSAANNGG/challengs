5891b3566caf293ee3fd8d72857fb7e6ced9dcad
ESSAANNGG <tkdwnsdk11@gmail.com>
2022-05-03 23:23:45 +0900

    하샤드합

---
+++ Hashad20220503.java
@@ -0,0 +1,16 @@
+public class Hashad20220503 {
+    class Solution {
+        public boolean solution(int x) {
+            boolean answer = true;
+            String number =  Integer.toString(x);
+
+            for(int i = 0; i < number.length(); i++){
+                String a1 = number.substring(i, i+1);
+                int a = Integer.parseInt(a1 + a1);
+                System.out.println(a+"=========");
+            }
+
+            return answer;
+        }
+    }
+}
